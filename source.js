189b49e894cda8613f1e259623c7ff070ff745d2
bapnator <80183828+bapnator@users.noreply.github.com>
2023-03-25 00:59:32 -0600

    Add files via upload

--- source.js
+++ source.js
@@ -1,17 +1,11 @@
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
-
-// Constantes para los posibles mensajes de estado
 const playerXWins = "Jugador X gana!";
 const playerOWins = "Jugador O gana!";
 const tie = "Empate";
-
-// Función para cambiar de jugador
 function changePlayer() {
   currentPlayer = currentPlayer === "X" ? "O" : "X";
 }
-
-// Función para verificar si un jugador ha ganado
 function checkForWinner() {
   const winningCombinations = [
     [0, 1, 2],
@@ -23,7 +17,6 @@ function checkForWinner() {
     [0, 4, 8],
     [2, 4, 6]
   ];
-
   for (let i = 0; i < winningCombinations.length; i++) {
     const [a, b, c] = winningCombinations[i];
     if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
@@ -32,33 +25,23 @@ function checkForWinner() {
   }
   return false;
 }
-
-// Función para verificar si el tablero está lleno
 function checkForTie() {
   return !gameState.includes("");
 }
-
-// Función para mostrar un mensaje de estado
 function displayMessage(message) {
   document.getElementById("message").textContent = message;
 }
-
-// Función para mostrar el tablero en la pantalla
 function displayBoard() {
   for (let i = 0; i < gameState.length; i++) {
     document.getElementById(i.toString()).textContent = gameState[i];
   }
 }
-
-// Función para reiniciar el juego
 function restartGame() {
   currentPlayer = "X";
   gameState = ["", "", "", "", "", "", "", "", ""];
   displayBoard();
   displayMessage("");
 }
-
-// Evento para manejar los clics de los cuadrados
 document.querySelectorAll(".square").forEach(square => {
   square.addEventListener("click", () => {
     const squareId = square.getAttribute("id");
@@ -77,9 +60,5 @@ document.querySelectorAll(".square").forEach(square => {
     }
   });
 });
-
-// Evento para manejar el botón de reinicio
 document.getElementById("restart").addEventListener("click", restartGame);
-
-// Mostrar el tablero inicial
 displayBoard();
